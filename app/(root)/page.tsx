a050cee867c9879acb7acc772a6f2e0dbd1b9e80
JamshedShaukat <168329984+JamhsedShaukat@users.noreply.github.com>
2024-07-22 18:20:06 +0500

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { SignOutButton,SignedIn, SignedOut, } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p>
+        Home Page
+      </p>
+      </div>
+  )
+}
+
+export default Home
